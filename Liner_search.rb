744de8d69539dde74ce231678ff756ef00d12248
Marc Steven <zhaoxinqiang328@163.com>
2017-10-03 20:25:07 +0800

    upload the search algorithms
    Upload the algorithms about search

---
+++ Liner_search.rb
@@ -0,0 +1,11 @@
+def liner_search(array,value)
+  array.each do |element|
+    if element == value
+      return value
+      elsif element > value
+      break
+      end
+
+    end
+  return nil
+  end
